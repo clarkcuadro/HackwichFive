5a245894715e4c082e6b930dc6c79f31f9e308a8
CM Student <cmstudent@uhwo-acm-09.local>
2018-02-26 11:06:03 -1000

    committed HackwichFive

--- HackwichFive/ViewController.swift
+++ HackwichFive/ViewController.swift
@@ -8,11 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+//define an array variable using an array literal with 5 items of type string
+    
+    var listArray = ["house", "car", "tree", "cat", "dog"]
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //set VC as delegate
+        //self.tableView.datasource = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +28,27 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return listArray.count
+    
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        //set up cell to display items in studentArray
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+        let text = listArray[indexPath.row]
+        cell.textLabel?.text = text
+        return cell
+    
+    
+    
+    }
+    
+    
+    
+    
+    
 }
+
 
